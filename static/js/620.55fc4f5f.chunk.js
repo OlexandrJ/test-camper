781c7184bc04d600eb3cdac37a9c805687ab601f
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2024-04-21 12:20:14 +0000

    Deploying to gh-pages from @ OlexandrJ/test-camper@8d55bb466161037ba51e8cf56365099cf29029f3 🚀

---
+++ static/js/620.55fc4f5f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_camper=self.webpackChunktest_camper||[]).push([[620],{2620:(t,e,L)=>{L.d(e,{rKS:()=>r});var a=L(3441);function r(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M5,6 L1,4.5 L1,18.443038 L12,23 L23,18.443038 L23,4 L19,6 M5,16 L5,2 L12,5 L19,2 L19,16 L12,19 L5,16 Z M11.95,5 L11.95,19"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=620.55fc4f5f.chunk.js.map
